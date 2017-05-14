d8a92d314ff929ba56ad214ede1e05a1d6dbdf03
Gisela Kottmeier <giselakottmeier@Giselas-MacBook-Pro.local>
2017-05-13 20:23:22 -0700

    Updated to work locally as well

--- controllers/sms.js
+++ controllers/sms.js
@@ -1,8 +1,18 @@
-// const apiKeys = require("../apikeys.js");
+var accountSid;
+var authToken;
 
-// Twilio Credentials
-const accountSid = process.env.accountSid;
-const authToken = process.env.authToken;
+if (process.env.NODE_ENV === 'production') {
+	console.log(process.env.NODE_ENV)
+	// Twilio Credentials from deployment
+	accountSid = process.env.accountSid;
+	authToken = process.env.authToken;
+} else {
+	const apiKeys = require("../apikeys.js");
+	// Twilio Credentials from local env
+	accountSid = apiKeys.accountSid;
+	authToken = apiKeys.authToken;
+}
+
 // require the Twilio module and create a REST client
 const client = require('twilio')(accountSid, authToken);
 
@@ -15,4 +25,15 @@ exports.sendText = function (to, from, body, mediaUrl, callback) {
 	    mediaUrl: mediaUrl,
 	  })
 	  .then((err) => callback(err));
+}
+
+exports.retrieveConvo = function (to, from) {
+	var currentConvo = {
+	    to: to,
+	    from: from
+	};
+
+	client.messages.list(currentConvo, function(err, data) {
+	    console.log(data);
+	});
 }
